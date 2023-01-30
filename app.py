a64f22d4f8ff1e4c0e2af9197d870517c6f00dca
Raphael <raphamontana@gmail.com>
2023-01-30 18:24:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from rdkit import Chem
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return Chem.MolToMolBlock(Chem.MolFromSmiles('C1CCC1'))
+    #return 'Hello, World!'
